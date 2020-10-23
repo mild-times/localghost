8d5b33055c0410e354614c089aac745a72ba4272
Yoshua Wuyts <yoshuawuyts@gmail.com>
2020-10-23 02:17:13 +0200

    format counter example more

--- examples/counter/src/lib.rs
+++ examples/counter/src/lib.rs
@@ -9,31 +9,30 @@ async fn main() {
     // TODO: this should be part of the macro.
     localghost::log::Logger::new().start().unwrap_throw();
 
-    let count = Rc::new(RefCell::new(0isize));
+    let counter = Rc::new(RefCell::new(0isize));
     let body = localghost::document().body();
 
     let button = Element::with_text(ElementKind::Button, "+");
-    let count_handle = count.clone();
+    let count_handle = counter.clone();
     button.on("click", move |_| {
         let mut handle = count_handle.borrow_mut();
         *handle += 1;
-        let counter = query_selector("#counter").unwrap_throw();
-        counter.set_text_content(Some(&format!("{}", *handle)))
+        let el = query_selector("#counter").unwrap_throw();
+        el.set_text_content(Some(&format!("{}", *handle)))
     });
     body.append_child(button);
 
-
-    let p = Element::with_text(ElementKind::P, &format!("{}", count.borrow()));
+    let p = Element::with_text(ElementKind::P, &format!("{}", counter.borrow()));
     p.set_attribute("id", "counter");
     body.append_child(p);
 
     let button = Element::with_text(ElementKind::Button, "-");
-    let count_handle = count.clone();
+    let count_handle = counter.clone();
     button.on("click", move |_| {
         let mut handle = count_handle.borrow_mut();
         *handle -= 1;
-        let counter = query_selector("#counter").unwrap_throw();
-        counter.set_text_content(Some(&format!("{}", *handle)));
+        let el = query_selector("#counter").unwrap_throw();
+        el.set_text_content(Some(&format!("{}", *handle)));
     });
     body.append_child(button);
 }
