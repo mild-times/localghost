d61dd68e3c42bc4db6d0ac07178f9a7a0180a08b
Yoshua Wuyts <yoshuawuyts+github@gmail.com>
2020-10-24 19:57:51 +0200

    add Request headers

--- src/net/request.rs
+++ src/net/request.rs
@@ -3,14 +3,16 @@ use std::io;
 
 use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::JsFuture;
-use web_sys::RequestInit;
 
 use crate::net::Response;
+use crate::prelude::*;
+use crate::utils::ResultExt;
 
 /// An HTTP Fetch Request.
 #[derive(Debug)]
 pub struct Request {
-    init: RequestInit,
+    headers: web_sys::Headers,
+    init: web_sys::RequestInit,
     url: String,
 }
 
@@ -22,27 +24,52 @@ impl Request {
         Self {
             init,
             url: url.to_owned(),
+            headers: web_sys::Headers::new().unwrap_throw(),
         }
     }
 
+    /// Get a header.
+    pub fn header(&self, name: &str) -> Option<String> {
+        self.headers.get(name).unwrap_throw()
+    }
+
+    /// Insert a header into the request.
+    pub fn insert_header(&self, name: &str, val: &str) {
+        self.headers.append(name, val).unwrap_throw();
+    }
+
+    /// Check whether the request contains a header.
+    pub fn contains_header(&self, name: &str) -> bool {
+        self.headers.has(name).unwrap_throw()
+    }
+
+    /// Remove a header from the `Request`.
+    ///
+    /// # Implementation Note
+    ///
+    /// Unlike other ecosystem crates this does not
+    pub fn remove_header(&self, name: &str) {
+        self.headers.delete(name).unwrap_throw();
+    }
+
     /// Submit a request
-    pub async fn send(self) -> Result<Response, io::Error> {
+    ///
+    /// # Errors
+    ///
+    /// An error may be returned if the underlying connection errors out.
+    pub async fn send(mut self) -> Result<Response, io::Error> {
+        // Attach the headers to the request data.
+        self.init.headers(self.headers.as_ref());
+
         // Send the request.
         let window = crate::utils::window();
         let request = web_sys::Request::new_with_str_and_init(&self.url, &self.init).unwrap();
         let promise = window.fetch_with_request(&request);
-        let resp = JsFuture::from(promise).await.unwrap();
+        let resp = JsFuture::from(promise)
+            .await
+            .err_kind(io::ErrorKind::Other)?;
         debug_assert!(resp.is_instance_of::<web_sys::Response>());
         let res: web_sys::Response = resp.dyn_into().unwrap();
-
-        // Get the response body.
-        // let promise = res.array_buffer().unwrap();
-        // let resp = JsFuture::from(promise).await.unwrap();
-        // debug_assert!(resp.is_instance_of::<js_sys::ArrayBuffer>());
-        // let buf: ArrayBuffer = resp.dyn_into().unwrap();
-        // let slice = Uint8Array::new(&buf);
-        // let mut body: Vec<u8> = vec![0; slice.length() as usize];
-        // slice.copy_to(&mut body);
 
         Ok(Response::new(res))
     }
