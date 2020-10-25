666b38229d8e4790bed5c688dd4c5f8d16db3309
Yoshua Wuyts <yoshuawuyts+github@gmail.com>
2020-10-25 17:53:07 +0100

    rm fetch

--- src/net/fetch.rs
+++
@@ -1,9 +0,0 @@
-use std::fmt::{self, Debug};
-use std::io;
-
-use js_sys::{Array, ArrayBuffer, Reflect, Uint8Array};
-use wasm_bindgen::JsCast;
-use wasm_bindgen_futures::JsFuture;
-use web_sys::RequestInit;
-
-use std::iter::{IntoIterator, Iterator};
