5d0d748836e6e9a2118c486ee78514dd5ec36c84
Yoshua Wuyts <yoshuawuyts@gmail.com>
2020-10-29 15:06:31 +0100

    Update history API

--- src/history.rs
+++ src/history.rs
@@ -6,82 +6,56 @@
 //!
 //! Notes: https://yoshuawuyts-old.netlify.com/history
 
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
 use crate::prelude::*;
 
+use async_std::prelude::Stream;
+use pin_project::pin_project;
 use wasm_bindgen::JsValue;
-
-// use std::cell::RefCell;
-// use std::sync::atomic::{AtomicBool, Ordering};
-
-// const EXISTS: RefCell<AtomicBool> = RefCell::new(AtomicBool::new(false));
 
 /// The Web History API.
 ///
-/// This provides a structured view onto the browser's history stack.
+/// The History API represents the URL and forward/backward buttons in the
+/// browser. It's modeled as a stack where URLs can be pushed/popped, and a
+/// cursor can be moved forward/backward along the stack.
 ///
-/// # Cursor
-///
-/// The state of the history API is kept in the browser's "history stack". We can
-/// move forward, backward, and modify the stack freely from Rust. As such the
-/// `History` type behaves itself as an `Iterator`, and is stateful.
-///
-/// The different methods on this type modify the history stack's cursor
-/// differently. As such each method documents how it behaves.
-///
-/// # TODO
-///
-/// We should reuse channels internally to improve performance, and on each
-/// navigation we should pass a unique ID (probably some unique prefix + globally
-/// incrementing number). That way we can associate an async fn with the right
-/// corresponding location.
-///
-/// Probably to interop with other navigators, we should ensure that external
-/// events store a location as well. If we see navigation event without a state,
-/// we should assign it one. This can race though.
-///
-/// # References
-///
-/// - https://html.spec.whatwg.org/multipage/history.html#shared-history-push/replace-state-steps
-/// - https://stackoverflow.com/questions/4570093/how-to-get-notified-about-changes-of-the-history-via-history-pushstate/4585031#4585031
+/// `History` also implements `Stream` which returns a url every time the
+/// cursor is moved along the stack. The only time this event is not triggered is
+/// when the cursor moves because of a new entry being pushed onto the stack.
+#[pin_project]
 #[derive(Debug)]
 pub struct History {
     inner: web_sys::History,
+    #[pin]
+    listener: Option<crate::events::EventStream>,
 }
 
 impl History {
     /// Create a new instance of `History`.
-    ///
-    /// # Cursor
-    ///
-    /// The cursor is initialized at the end of the stack.
     pub fn new() -> Self {
-        // assert_eq!(
-        //     EXISTS
-        //         .borrow()
-        //         .compare_and_swap(false, true, Ordering::SeqCst),
-        //     false,
-        //     "Only one History instance can be created per application"
-        // );
-
         Self {
             inner: crate::utils::window().history().unwrap_throw(),
+            listener: None,
         }
     }
 
-    /// Returns the number of history events in the stack.
-    ///
-    /// # Cursor
-    ///
-    /// The cursor does not move.
+    /// Returns the number of urls in the History stack.
     pub fn len(&self) -> usize {
         self.inner.length().unwrap_throw() as usize
     }
 
+    /// Get the current url from the History stack.
+    pub fn current(&self) -> String {
+        crate::utils::document()
+            .location()
+            .unwrap_throw()
+            .href()
+            .unwrap_throw()
+    }
+
     /// Push a new url onto the history stack.
-    ///
-    /// # Cursor
-    ///
-    /// This moves the cursor forward.
     pub fn push(&self, url: &str) {
         let null = JsValue::null();
         self.inner
@@ -90,21 +64,11 @@ impl History {
     }
 
     /// Pop a url from the history stack.
-    ///
-    /// # Cursor
-    ///
-    /// This moves the history cursor backward.
-    pub async fn pop(&self) {
-        let fut = crate::utils::window().once("popstate");
+    pub fn pop(&self) {
         self.inner.back().unwrap_throw();
-        fut.await;
     }
 
     /// Replace the url currently on the stack with another url.
-    ///
-    /// # Cursor
-    ///
-    /// This keeps the cursor in-place.
     pub fn replace(&self, url: &str) {
         let null = JsValue::null();
         self.inner
@@ -112,27 +76,58 @@ impl History {
             .unwrap_throw();
     }
 
-    /// Move the cursor forward in time.
+    /// Move the cursor forwards and backwards through the History stack
+    /// depending on the value of the parameter.
     ///
-    /// # Cursor
-    ///
-    /// This moves the history cursor forward.
-    pub async fn forward(&self) {
+    /// Passing a value of `0` will reload the page.
+    pub async fn move_by(&self, delta: i32) {
+        let fut = crate::utils::window().once("popstate");
+        self.inner.go_with_delta(delta).unwrap_throw();
+        fut.await;
+    }
+
+    /// Moves the cursor to the next url in the History stack.
+    pub async fn move_next(&self) {
         let fut = crate::utils::window().once("popstate");
         self.inner.forward().unwrap_throw();
         fut.await;
     }
 
-    // fn len
-    // fn forward
-    // fn backward
-    // fn go
+    /// Moves the cursor to the previous url in the History stack.
+    pub async fn move_prev(&self) {
+        let fut = crate::utils::window().once("popstate");
+        self.inner.back().unwrap_throw();
+        fut.await;
+    }
+
+    /// Reload the current url, like the Refresh button.
+    pub async fn reload(&self) {
+        let fut = crate::utils::window().once("popstate");
+        self.inner.go().unwrap_throw();
+        fut.await;
+    }
 }
 
-// impl Drop for History {
-//     fn drop(&mut self) {
-//         // Allow a new instance to be created once this no longer exists.
-//         EXISTS.borrow().store(false, Ordering::SeqCst);
-//     }
-// }
-// TODO: impl Stream & DoubleEndedStream for History
+impl Stream for History {
+    type Item = String;
+
+    /// Return URLs generated by moving the cursor on the stack. This is
+    /// equivalent to listening for the `"popstate"` event.
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        if self.listener.is_none() {
+            self.listener = Some(crate::utils::window().on("popstate"));
+        }
+
+        let listener = self.project().listener.get_mut().as_mut().unwrap_throw();
+        match Pin::new(listener).poll_next(cx) {
+            Poll::Ready(_) => Poll::Ready(Some(
+                crate::utils::document()
+                    .location()
+                    .unwrap_throw()
+                    .href()
+                    .unwrap_throw(),
+            )),
+            Poll::Pending => Poll::Pending,
+        }
+    }
+}
